11a29b760c708a9144d48cbe5f069a14deb00688
TriangleMesh <weibodeemail@proton.me>
2024-12-09 02:02:23 -0800

    fix bug

--- prev/adventure_timer.py
+++ prev/adventure_timer.py
@@ -1,111 +1,91 @@
-package main
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 27 18:24:04 2022
 
-import (
-	"fmt"
-	"net/http"
-	"os"
-	"strconv"
-	"strings"
-	"time"
+@author: TriangleMesh
+"""
 
-	"github.com/gin-gonic/gin"
-)
+import time
+import datetime
+import vlc
+import os
 
-// 定义音乐播放函数 (此处仅做占位，实际需要整合音频播放库)
-func playMusic(musicName string) {
-	fmt.Println("Playing:", musicName)
-}
 
-// 倒计时函数
-func countdown(h, m, s int) {
-	totalSeconds := h*3600 + m*60 + s
-	for totalSeconds > 0 {
-		timer := fmt.Sprintf("%02d:%02d:%02d", totalSeconds/3600, (totalSeconds%3600)/60, totalSeconds%60)
-		fmt.Printf("\r%s", timer)
-		time.Sleep(1 * time.Second)
-		totalSeconds--
-	}
-	fmt.Println()
-}
 
-// 带音乐播放的倒计时
-func countdownWithPlayMusic(h, m, s int, musicName string) {
-	countdown(h, m, s)
-	playMusic(musicName)
-}
+media_player = vlc.MediaPlayer()
+class_begins = vlc.Media("MusicNameForClassBegins")
+class_ends = vlc.Media("MusicNameForClassEnds")
 
-// 定义主逻辑
-func timeLoop(c *gin.Context, h1, m1, s1, h2, m2, s2, totalH, totalM, totalS int) {
-	totalInputTime := totalH*3600 + totalM*60 + totalS
-	timeDuration := h1*3600 + m1*60 + s1 + h2*3600 + m2*60 + s2
-	num := totalInputTime / timeDuration
+# collect input
+print("\nHi, welcome back! So excited to see you again!\n")
+total_time = input("How long would you like to take adventure? (hh mm ss)\n")
+t_h, t_m, t_s = total_time.split(' ')
+choose_time = int(input("\nChoose between: \n1) day mode: 40m + 10m \n2) night mode: 1h20m + 15m \n3) others\nPlease type in number only. \n"))
 
-	for i := 1; i <= num; i++ {
-		countdownWithPlayMusic(h1, m1, s1, "MusicNameForClassEnds")
-		if i < num {
-			countdownWithPlayMusic(h2, m2, s2, "MusicNameForClassBegins")
-		}
-	}
+#def
+def countdown(h,m,s):
+    total_seconds = h * 3600 + m * 60 + s
+    while total_seconds > 0:
+        timer = datetime.timedelta(seconds = total_seconds)
+        print(timer, end = "\r", flush=True)
+        time.sleep(1)
+        total_seconds -= 1
+def countdown_with_play_music(h_with_music, m_with_music, s_with_music, music_name):
+    countdown(h_with_music, m_with_music, s_with_music)
+    media_player.set_media(music_name)
+    media_player.play()
+    os.system('clear')      
+def print_i(i):
+    if i > 1:
+        print("Hooray! You have beaten", i, "enemies already. Let's have a rest! ")
+    elif i == 1:
+        print("Hooray! You have beaten 1 enemy already. Let's have a rest! ")
+def print_remaining_i(remaining_i):
+    if remaining_i == 1:
+        print("Only 1 enemy is left. Fight until the end!\n")
+    elif remaining_i > 1:
+        print("Only", remaining_i, "enemies are left. Fight until the end!\n")
+def time_loop(h1, m1, s1, h2, m2, s2): 
+    i = 1
+    total_input_time = int(t_h) * 3600 + int(t_m) * 60 + int(t_s)
+    time_duration = 3600 * (h1+h2) + 60 * (m1 + m2) + (s1 + s2)
+    num = total_input_time//time_duration
+    os.system('clear')
+    if num == 1:
+        print("Merely 1 enemy this time. Hold on your weapon. Let's begin! \n")
+    if num > 1:
+        print("There are", num, "enemies coming. Hold on your weapon. Let's begin! \n")
+    while i < num:
+        remaining_i = num - i
+        countdown_with_play_music(h1, m1, s1, class_ends)
+        print_i(i)
+        countdown_with_play_music(h2, m2, s2, class_begins)
+        print("Adventure begins! ")
+        print_remaining_i(remaining_i)
+        i = i+1
+    countdown_with_play_music(h1, m1, s1, class_ends)
+    media_player.set_media(class_ends)
+    media_player.play()
+    # os.system('clear')
+    if num == 1:
+        print("Wow! You have beaten 1 enemy this time. ")
+    if num > 1:
+        print("Wow! You have beaten", num, "enemies this time. ")
+    print("Adventure ends. May you have an energetic and peaceful day! \nLooking forward to see you again! \n")
 
-	c.JSON(http.StatusOK, gin.H{
-		"message": fmt.Sprintf("Adventure ends. You defeated %d enemies!", num),
-	})
-}
+# execute code
+if choose_time == 1:
+    time_loop(0, 40, 0, 0, 10, 0)
+    # time_loop(0, 0, 10, 0, 0, 10)
+if choose_time == 2:
+    time_loop(0, 80, 0, 0, 15, 0)
+if choose_time == 3:
+    lesson_time = input("\nPlease type the adventure duration. (hh mm ss)\n")
+    l_h1, l_m1, l_s1 = lesson_time.split(' ')
+    lh1, lm1, ls1 = int(l_h1), int(l_m1), int(l_s1)
+    break_time = input("\nPlease type the break time. (hh mm ss)\n")
+    b_h2, b_m2, b_s2 = break_time.split(' ')
+    bh2, bm2, bs2 = int(b_h2), int(b_m2), int(b_s2)
+    time_loop(lh1, lm1, ls1, bh2, bm2, bs2)
 
-// 启动 Gin Web 应用程序
-func main() {
-	r := gin.Default()
-
-	r.POST("/start", func(c *gin.Context) {
-		totalTime := c.PostForm("total_time")
-		mode := c.PostForm("mode")
-
-		// 解析输入的总时间
-		times := strings.Split(totalTime, " ")
-		if len(times) != 3 {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid time format. Use 'hh mm ss'"})
-			return
-		}
-
-		totalH, _ := strconv.Atoi(times[0])
-		totalM, _ := strconv.Atoi(times[1])
-		totalS, _ := strconv.Atoi(times[2])
-
-		// 解析模式
-		switch mode {
-		case "1": // Day mode: 40m + 10m
-			timeLoop(c, 0, 40, 0, 0, 10, 0, totalH, totalM, totalS)
-		case "2": // Night mode: 1h20m + 15m
-			timeLoop(c, 1, 20, 0, 0, 15, 0, totalH, totalM, totalS)
-		case "3": // 自定义模式
-			lessonTime := c.PostForm("lesson_time")
-			breakTime := c.PostForm("break_time")
-
-			lesson := strings.Split(lessonTime, " ")
-			breakT := strings.Split(breakTime, " ")
-			if len(lesson) != 3 || len(breakT) != 3 {
-				c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid lesson or break time format. Use 'hh mm ss'"})
-				return
-			}
-
-			h1, _ := strconv.Atoi(lesson[0])
-			m1, _ := strconv.Atoi(lesson[1])
-			s1, _ := strconv.Atoi(lesson[2])
-
-			h2, _ := strconv.Atoi(breakT[0])
-			m2, _ := strconv.Atoi(breakT[1])
-			s2, _ := strconv.Atoi(breakT[2])
-
-			timeLoop(c, h1, m1, s1, h2, m2, s2, totalH, totalM, totalS)
-		default:
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid mode. Choose 1, 2, or 3"})
-		}
-	})
-
-	// 启动服务器
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	r.Run(":" + port)
-}
